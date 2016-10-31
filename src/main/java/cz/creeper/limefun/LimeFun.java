35868d37eec7089c66e26bdebddf5b64648cbd16
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-10-31 21:50:33 +0100

    remove unused code

--- src/main/java/cz/creeper/limefun/LimeFun.java
+++ src/main/java/cz/creeper/limefun/LimeFun.java
@@ -8,26 +8,12 @@ import cz.creeper.limefun.pipe.PipeSystem;
 import lombok.Getter;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.block.BlockType;
-import org.spongepowered.api.block.BlockTypes;
-import org.spongepowered.api.block.tileentity.TileEntity;
-import org.spongepowered.api.block.tileentity.carrier.TileEntityCarrier;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.manipulator.mutable.block.DirectionalData;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppedServerEvent;
-import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.format.TextStyles;
-import org.spongepowered.api.util.Direction;
-import org.spongepowered.api.world.Location;
 
-import java.util.Optional;
 import java.util.Random;
 
 @Plugin(
@@ -60,54 +46,5 @@ public class LimeFun {
         logger.info("Disabling LimeFun...");
         pipeSystem.stop();
         logger.info("LimeFun disabled.");
-    }
-
-    @Listener
-    public void onPlayerJoin(ClientConnectionEvent.Join event,
-                             @org.spongepowered.api.event.filter.Getter("getTargetEntity") Player player) {
-        player.sendMessage(Text.of(TextColors.AQUA, TextStyles.BOLD, "Hi " + player.getName()));
-    }
-
-    private boolean isValid(Location location) {
-        if (location.getBlockType() == BlockTypes.DISPENSER) {
-            DirectionalData directionData = (DirectionalData) location.get(DirectionalData.class).get();
-            Direction direction = directionData.get(Keys.DIRECTION).get();
-            Location facingBlock = location.getRelative(direction);
-
-            System.out.println(direction + " " + facingBlock);
-            return facingBlock.getBlockType() == BlockTypes.STAINED_GLASS;
-        }
-
-        return false;
-    }
-
-    private Direction getNextDirection(Location enteringBlock, Direction enteringDirection) {
-        Location frontBlock = enteringBlock.getRelative(enteringDirection);
-        BlockType enteringBlockType = enteringBlock.getBlockType();
-
-        if(frontBlock.getBlockType() == enteringBlockType) {  // TODO: Check for byte data
-            return enteringDirection;
-        }
-
-        Optional<TileEntity> optionalFrontTileEntity = frontBlock.getTileEntity();
-
-        if(optionalFrontTileEntity.isPresent()) {
-            TileEntity frontTileEntity = optionalFrontTileEntity.get();
-
-            if(frontTileEntity instanceof TileEntityCarrier) {
-                return enteringDirection;  // Possibly can enter the tile entity's inventory
-                /*
-                TileEntityCarrier frontCarrier = (TileEntityCarrier) frontTileEntity;
-                TileEntityInventory<TileEntityCarrier> inventory = frontCarrier.getInventory();
-
-                if(inventory.)
-                if(inventory.offer(ItemStack.builder().build()).getType() == InventoryTransactionResult.Type.SUCCESS) {
-                    return enteringDirection;  //
-                }
-                 */
-            }
-        }
-
-        return enteringDirection.getOpposite();
     }
 }
